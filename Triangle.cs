3939cfd08691145da7d7f2db20f6593a889282af
amitthapliyal113@gmail.com <amitthapliyal113@gmail.com>
2024-08-12 16:53:54 +0530

    first commit

---
+++ Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the height of the triangle: ");
+        int height = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= height; i++)
+        {
+            Console.Write(new string(' ', height - i)); // Print spaces
+            Console.WriteLine(new string('*', 2 * i - 1)); // Print stars
+        }
+    }
+}
